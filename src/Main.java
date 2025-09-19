dc435e5c657e275314752a7010a488901712b145
Nicole0509 <niashavril0509@gmail.com>
2025-09-19 08:39:32 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Matrices In Java!!");
+
+        int[][] matrices = new int[][]{
+                {1,2,3},
+                {4,5,6},
+                {7,8,9},
+                {10,11,12}
+        };
+
+
+    }
+}
