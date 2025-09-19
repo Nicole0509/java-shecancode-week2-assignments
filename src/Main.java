fc0f603282db093445eed77450041b5c5df1a9aa
Nicole0509 <niashavril0509@gmail.com>
2025-09-19 09:02:34 +0200

    Created a function to display matrices

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,34 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-
-
-        System.out.println("Matrices In Java!!");
-
-        int[][] matrices = new int[][]{
-                {1,2,3},
-                {4,5,6},
-                {7,8,9},
-        };
-
-
+    public static void displayMatrix(int[][] matrices){
         for(int[] matrix : matrices){
             for(int element : matrix){
                 System.out.print(element + "\t");
             }
             System.out.println();
         }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Matrices In Java!!");
+
+        int[][] matrix1 = new int[][]{
+                {1,2},
+                {4,5},
+        };
+
+        int[][] matrix2 = new int[][]{
+                {3,6},
+                {7,8},
+        };
+
+        displayMatrix(matrix1);
+        displayMatrix(matrix2);
+
     }
 }
